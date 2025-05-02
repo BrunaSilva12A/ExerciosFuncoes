eeaf1f982f2d5915b0e5cff193be474c805f7810
Bruna Silva <brunasilvamarques09@gmail.com>
2025-05-02 17:57:33 -0300

    resolução do exercício extremamente básico

---
+++ pages/Extremamente Basico/extremamente_basico.js
@@ -0,0 +1,15 @@
+/* Crie uma função que recebe dois valores inteiros como parametros Efetue a soma dos dois valores e retorne o resultado. Imprima os resultados dos três testes conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado.*/
+
+function somar(valor1, valor2){
+    return valor1 + valor2
+}
+
+let teste1 = somar(10, 9)
+let teste2 = somar(-10, 4)
+let teste3 = somar(15, -7)
+
+console.log(`
+    X1 = ${teste1}
+    X2 = ${teste2}
+    X3 = ${teste3}
+    `)
